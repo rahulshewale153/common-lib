642ebf9bb2c5ca67c6abe984ec469b08655c7cfa
Rahul Shewale <rahuls@lemmatechnologies.com>
2024-10-06 18:36:10 +0530

    update lib

--- driver/postgres/postgres.go
+++ driver/postgres/postgres.go
@@ -1,10 +1,7 @@
 package postgres
 
 import (
-	"bytes"
-	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/rahulshewale153/common-lib/log"
@@ -27,13 +24,11 @@ const (
 	SQLTimeFormatLayout = "2006-01-02 15:04:05"
 )
 
-type MysqlConnector struct {
+type PGConnector struct {
 	client *sql.DB
 }
-type MysqlTxConnector struct {
-	tx *sql.Tx
-}
-type MySQLConfig struct {
+
+type PGConfig struct {
 	Host             string
 	Port             int
 	UserName         string
@@ -51,21 +46,16 @@ type MySQLConfig struct {
 	ParseTime        bool
 }
 
-// return MysqlConnector with setting client to given connection
+// return PGConnector with setting client to given connection
 // its expected to provide mocked conn while calling this api
-func NewMockedMySQLConnector(conn *sql.DB) *MysqlConnector {
-	return &MysqlConnector{
+func NewMockedPGConnector(conn *sql.DB) *PGConnector {
+	return &PGConnector{
 		client: conn,
-	}
-}
-func NewMockedMyTransactionConnector(transation *sql.Tx) *MysqlTxConnector {
-	return &MysqlTxConnector{
-		tx: transation,
 	}
 }
 
 // return the new MySQL connector
-func NewMySQLConnector(cfg MySQLConfig) (*MysqlConnector, error) {
+func NewPGConnector(cfg PGConfig) (*PGConnector, error) {
 	collation := cfg.Collation
 	if "" == collation {
 		collation = defaultCollation
@@ -141,90 +131,25 @@ func NewMySQLConnector(cfg MySQLConfig) (*MysqlConnector, error) {
 	}
 	conn.SetConnMaxLifetime(maxLifetime)
 
-	return &MysqlConnector{
+	return &PGConnector{
 		client: conn,
 	}, nil
 }
 
 // execute the select queries
-func (conn *MysqlConnector) ExecuteSelect(query string, args ...any) (*sql.Rows, error) {
+func (conn *PGConnector) ExecuteSelect(query string, args ...any) (*sql.Rows, error) {
 	return conn.client.Query(query, args...)
 }
 
-func (conn *MysqlConnector) Execute(query string, args ...any) (sql.Result, error) {
+func (conn *PGConnector) Execute(query string, args ...any) (sql.Result, error) {
 	return conn.client.Exec(query, args...)
 }
 
 // close the Aerospike client connection
-func (conn *MysqlConnector) Close() {
+func (conn *PGConnector) Close() {
 	if err := conn.client.Close(); err != nil {
 		log.Error("Error while closing MySQL connection!!! ", err.Error())
 	} else {
 		log.Error("Closed MySQL connection!!!")
 	}
-}
-
-// to use this function ensure that mysql connection user have
-// select privileges on  INFORMATION_SCHEMA.PARTITIONS
-// retrun true if data is updated in the table
-func (conn *MysqlConnector) CheckTableUpdatedSince(database string, tables []string, time time.Time) (updated bool, err error) {
-	values := make([]any, 0)
-	values = append(values, database)
-	for _, table := range tables {
-		values = append(values, table)
-	}
-
-	placeHolder := strings.Repeat("?,", len(tables))
-	placeHolder = placeHolder[:len(placeHolder)-1]
-
-	values = append(values, time.Format(SQLTimeFormatLayout))
-	values = append(values, time.Format(SQLTimeFormatLayout))
-
-	var buff bytes.Buffer
-	buff.WriteString("SELECT UPDATE_TIME ")
-	buff.WriteString("FROM  INFORMATION_SCHEMA.PARTITIONS ")
-	buff.WriteString("WHERE TABLE_SCHEMA =  ?")
-	buff.WriteString(" AND TABLE_NAME IN (")
-	buff.WriteString(placeHolder)
-	buff.WriteString(")")
-	buff.WriteString(" AND (UPDATE_TIME >= ? OR CREATE_TIME >= ?)")
-
-	row, er := conn.ExecuteSelect(buff.String(), values...)
-	for {
-		if nil != er {
-			err = er
-			break
-		}
-
-		if row.Next() {
-			updated = true
-		}
-		break
-	}
-
-	if nil != row {
-		row.Close()
-	}
-
-	return
-}
-func (conn *MysqlConnector) BeginTx(ctx context.Context) (*MysqlTxConnector, error) {
-	tx, err := conn.client.BeginTx(ctx, nil)
-	if err != nil {
-		log.Error("Error while Begin Transaction!!! ", err.Error())
-		return nil, err
-	}
-	mysqlTxConn := &MysqlTxConnector{
-		tx: tx,
-	}
-	return mysqlTxConn, nil
-}
-func (tx *MysqlTxConnector) Commit() error {
-	return tx.tx.Commit()
-}
-func (tx *MysqlTxConnector) Rollback() error {
-	return tx.tx.Rollback()
-}
-func (tx *MysqlTxConnector) ExecTransaction(ctx context.Context, query string, args ...any) (sql.Result, error) {
-	return tx.tx.ExecContext(ctx, query, args...)
 }
